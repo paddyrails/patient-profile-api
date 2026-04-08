cb779dd2999fba6feb4d637ec5010fa4939b4446
paddy.awarenesstech <147popsy@gmail.com>
2026-04-08 08:03:04 -0400

    added rate limiting

--- api/v1/patient_profile_api.py
+++ api/v1/patient_profile_api.py
@@ -1,9 +1,10 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Request
 from sqlalchemy.orm import Session
 from common.database import get_db
 from common.schemas.patient_profile import PatientProfileCreate, PatientProfileUpdate, PatientProfileResponse
 from services.patient_profile_service import PatientProfileService
 from typing import List
+from common.limiter import limiter
 
 router = APIRouter(prefix="/api/v1/patient-profiles", tags=["Patient Profiles"]) 
 
@@ -11,12 +12,14 @@ service = PatientProfileService()
 
 # Get all Patient Profiles
 @router.get("/", response_model=List[PatientProfileResponse])
-def get_all(db: Session = Depends(get_db)):
+@limiter.limit("10/minute")
+def get_all(request: Request, db: Session = Depends(get_db)):
     return service.get_all(db)
 
 #Get Patient by id
 @router.get("/{id}", response_model=PatientProfileResponse)
-def get_by_id(id: int, db: Session = Depends(get_db)):
+@limiter.limit("10/minute")
+def get_by_id(request: Request, id: int, db: Session = Depends(get_db)):
     try:
         return service.get_by_id(db, id)
     except ValueError as e:
@@ -24,7 +27,8 @@ def get_by_id(id: int, db: Session = Depends(get_db)):
     
 #Create Patient profile
 @router.post("/", response_model=PatientProfileResponse, status_code=201)
-def create(data: PatientProfileCreate, db: Session = Depends(get_db)):
+@limiter.limit("3/minute")
+def create(request: Request, data: PatientProfileCreate, db: Session = Depends(get_db)):
     try:
         return service.create(db, data)
     except ValueError as e:
@@ -32,7 +36,8 @@ def create(data: PatientProfileCreate, db: Session = Depends(get_db)):
     
 #Update Patient Profile
 @router.put("/{id}", response_model=PatientProfileResponse)
-def update(id: int, data: PatientProfileUpdate, db: Session = Depends(get_db)):
+@limiter.limit("3/minute")
+def update(request: Request, id: int, data: PatientProfileUpdate, db: Session = Depends(get_db)):
     try:
         return service.update(db, id, data)
     except ValueError as e:
@@ -40,7 +45,8 @@ def update(id: int, data: PatientProfileUpdate, db: Session = Depends(get_db)):
     
 # Delete Patient Profile
 @router.delete("/{id}", status_code=200)
-def delete(id: int, db: Session = Depends(get_db)):
+@limiter.limit("3/minute")
+def delete(request: Request, id: int, db: Session = Depends(get_db)):
     try:
         return service.delete(db, id)
     except ValueError as e:
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from common.database import engine, Base
 from api import router
+from common.limiter import limiter  
+from slowapi.errors import RateLimitExceeded
+from slowapi import _rate_limit_exceeded_handler
 
 Base.metadata.create_all(bind=engine)
 
@@ -9,5 +12,8 @@ app = FastAPI(
     version="1.0.0",
     description="API for managing patient profiles"
 )
+
+app.state.limiter = limiter                                                                                                                  
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 app.include_router(router)
